f34f26dc4b88ffb668d9b99220f0fbd0e369cfe7
ecThor <flakonet@msn.com>
2023-04-17 20:30:55 -0500

    Agregado CORS

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import 'dotenv/config'
 import express from "express";
+import cors from 'cors'
 import './database/connectDB.js'
 import authRoutes from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
 
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 app.use(cookieParser())
